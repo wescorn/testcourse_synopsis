b285ebfef8ccbadac7ee190fa23ad56deb56aaa4
hlynur92 <43953815+hlynur92@users.noreply.github.com>
2022-12-30 00:54:14 +0100

    update on unit test
    added json file with test data and updated the test code.

--- src/tests/user.controller.tests/quiz.test.js
+++ src/tests/user.controller.tests/quiz.test.js
@@ -1,136 +1,38 @@
+import create_test_data01 from "../../test_data/create_quiz_test1.json"
+import jwt from 'jsonwebtoken';
+
 const request = require('supertest');
 
 const conn = require('../../config/sequelize-connect');
 const app = require('../../../app');
 
-let authorizationToken;
-
 describe('APIs Testing...', () => {
   test('Create Quiz', async () => {
+
+    const token = jwt.sign(
+        {
+            user: {
+                userId: 1,
+                email: "example@example.com",
+                createdAt: new Date(),
+            },
+        },
+        process.env.SECRET,
+    );
+
     const res = await request(app)
       .post('/api/quiz')
-      .send({
-        name: "This is my Awesome Quiz",
-        description: "This is the awesome quiz I created",
-        questions:[
-            {
-                text: "Which one is correct ?",
-                answers:[
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Me",
-                        isCorrect: true
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    }
-                ] 
-            },
-            {
-                text: "Which one is correct ?",
-                answers:[
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Me",
-                        isCorrect: true
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    }
-                ] 
-             },
-             {
-                text: "Which one is correct ?",
-                answers:[
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Me",
-                        isCorrect: true
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    }
-                ] 
-             },
-             {
-                text: "Which one is correct ?",
-                answers:[
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Me",
-                        isCorrect: true
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    }
-                ] 
-             },
-             {
-                text: "Which one is correct ?",
-                answers:[
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    },
-                    {
-                        text: "Me",
-                        isCorrect: true
-                    },
-                    {
-                        text: "Not Me",
-                        isCorrect: false
-                    }
-                ] 
-             }
-        ]
-    })
-    .set('x-token', authorizationToken);// Taking a Nap need to fix this so it has the token I assume
-    UserData = res.body.data;
+      .set('x-token', token)
+      .send(create_test_data01);
     expect(res.statusCode).toBe(200);
   });
 });
 
-afterAll(async () => {
-  try {
-    await conn.dbDisconnect();
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-});
+// afterAll(async () => {
+//   try {
+//     await conn.dbDisconnect();
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// });
